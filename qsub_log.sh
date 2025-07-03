66748bd162754bb1137a37546c7dcad978c8e1cc
Warface122 <fwwqeqweqfsaffaf@gmail.com>
2025-07-03 15:18:43 +0400

    Update qsub_log.sh

--- qsub_log.sh
+++ qsub_log.sh
@@ -6,21 +6,18 @@ find_percent() {
     local file="$1"
     SERVER=$(basename "$file" | awk -F'_' '{print $3}' | sed -E 's/(.*\.com).*/\1/')
 
-    ssh "$SERVER" "find /SCRATCH/$USERNAME/ -type f -name '*.log'" 2>/dev/null | while read -r log_file; do
-        # Считываем последнюю строку с символом %
-        ssh "$SERVER" "grep '%' \"$log_file\" | tail -n 1" | while read -r percent_line; do
-            echo "[$SERVER] $percent_line"
-        done
+    ssh "$SERVER" "find /SCRATCH/$USERNAME/-type f -name '*.log'" 2>/dev/null | while read -r log_file; do
+        scp "$SERVER:$log_file" /tmp/ > /dev/null
+        last_percent=$(grep '%' /tmp/$(basename "$log_file") | tail -n 1)
+        echo "[$SERVER] $last_percent"
     done
 }
 
-# Проверка, что скрипт запущен из папки qsub_log
 if [[ $(basename "$PWD") != "qsub_log" ]]; then
-    echo "❌ Пожалуйста, запустите скрипт из папки qsub_log!"
+    echo "❌ Запусти скрипт из папки qsub_log!"
     exit 1
 fi
 
-# Обработка всех файлов в текущей директории
 for file in *; do
     find_percent "$file" &
 done
